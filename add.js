d58691bb843d54b301f9a86207af6a73f2c1203d
Eby-Samuel693 <coquelet.samuel@gmail.com>
2021-02-05 10:15:08 +0100

    jyhgtrfe

---
+++ add.js
@@ -0,0 +1,30 @@
+let red = document.getElementById('red');
+let blue = document.getElementById('blue');
+let yellow = document.getElementById('yellow');
+let green = document.getElementById('green');
+let black = document.getElementById('black');
+
+function change1(){
+    document.body.style.backgroundColor = 'red';
+}
+
+function change2(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+function change3(){
+    document.body.style.backgroundColor = 'yellow';
+}
+
+function change4(){
+    document.body.style.backgroundColor = 'green';
+}
+
+function change5(){
+    document.body.style.backgroundColor = 'black';
+}
+red.addEventListener('click',change1);
+blue.addEventListener('click',change2);
+yellow.addEventListener('click',change3);
+green.addEventListener('click',change4);
+black.addEventListener('click',change5);
---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <p>menu deroulant \/</p>
+        <nav class="touche">
+            <ul>
+                <li>rouge</li>
+                <button id="red">ici</button>
+                <li>bleu</li>
+                <button id="blue">ici</button>
+                <li>jaune</li>
+                <button id="yellow">ici</button>
+                <li>vert</li>
+                <button id="green">ici</button>
+                <li>noir</li>
+                <button id="black">ici</button>
+            </ul>
+        </nav>
+    </div>
+
+    <script src="add.js"></script>
+</body>
+</html>
